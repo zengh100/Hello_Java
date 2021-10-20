34ae7909eb142ed2affce9b03f88c4ff584d6572
hrzenger <zengh100@gmail.com>
2021-10-20 15:06:55 -0700

    Fix error (voide should be void) in Hello.java

--- Hello.java
+++ Hello.java
@@ -1,5 +1,5 @@
 public class Hello {
-  public static voide main(string[] args) {
+  public static void main(string[] args) {
     // Print "Hello, World" in the terminal window
     System.out.println("Hello, World");
   }
