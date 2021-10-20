bfca6154c412c48f141593e241660b75b4361f27
hrzenger <zengh100@gmail.com>
2021-10-20 15:14:27 -0700

    Fix error (string should be String) in Hello.java

--- Hello.java
+++ Hello.java
@@ -1,5 +1,5 @@
 public class Hello {
-  public static void main(string[] args) {
+  public static void main(String[] args) {
     // Print "Hello, World" in the terminal window
     System.out.println("Hello, World");
   }
